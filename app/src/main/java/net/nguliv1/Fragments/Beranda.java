d8eef25fb24ab4f51af408c9a4a2937d6a70271a
Iqbal <aodihis@gmail.com>
2016-06-15 05:18:43 +0700

    Navigation bar.

---
+++ app/src/main/java/net/nguliv1/Fragments/Beranda.java
@@ -0,0 +1,31 @@
+package net.nguliv1.Fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import net.nguliv1.R;
+
+
+public class Beranda extends Fragment{
+
+    public Beranda() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.beranda, container, false);
+    }
+
+}
